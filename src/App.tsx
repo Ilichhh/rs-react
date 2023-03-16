3853f823bd3331336383fe68f524feac4016cd5f
ilya <shakurov.ia@gmail.com>
2023-03-16 17:11:42 +0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import Error from './pages/Error';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
