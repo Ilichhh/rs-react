67afc300a24de7228c1c2d9b9a7b8324b7c8d5ee
ilya <shakurov.ia@gmail.com>
2023-03-16 19:19:16 +0300

    feat: add sass and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,25 @@ import { Routes, Route, Link } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import Error from './pages/Error';
+import './App.scss';
 
 function App() {
   return (
-    <>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/about">About Us</Link>
+    <div className="container">
+      <header className="header">
+        <Link className="header__link" to="/">
+          Home
+        </Link>
+        <Link className="header__link" to="/about">
+          About Us
+        </Link>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
